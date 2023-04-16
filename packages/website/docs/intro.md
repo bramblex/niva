6d84e9f4455c62f1c8d1b0a1b59b58b0e905728e
brambles <qjnight@gmail.com>
2023-04-17 00:10:27 +0800

    doc: intro

--- packages/website/docs/intro.md
+++ packages/website/docs/intro.md
@@ -6,4 +6,31 @@ sidebar_position: 1
 
 ## 什么是 Niva？
 
-Niva（发音为 /'niːvə/，谐音“泥娃”）是一个类似 [Electron](https://www.electronjs.org/) 的用前端技术开发桌面应用的框架。
+Niva 是一个跨平台（支持 Windows 和 MacOS）的桌面应用开发框架，使用前端技术开发，可用于构建轻量级的桌面应用。它基于系统 Webview 而非 Chromium，体积仅为 3MB ~ 4MB，不需要 Node.js 环境，直接提供系统 API 进行操作。
+
+## Niva 开发者工具
+
+Niva 开发者工具是一个图形化界面开发者工具，提供图形化界面进行对 Niva 项目进行构建或者调试的开发工具。
+
+## Niva 与 Tauri 和 Electron 的异同
+
+Niva 与 Tauri 和 Electron 定位不同。它是 Tauri 和 Electron 的轻量级替代方案，比它们更加简单易用但同时牺牲了部分生态和能力。
+
+下表列出了 Niva、Tauri 和 Electron 的主要区别：
+
+|          | Niva                       | Tauri       | Electron       |
+| -------- | -------------------------- | ----------- | -------------- |
+| 体积     | 3MB                        | 6MB+        | 85MB+          |
+| APP 后端 | 无，但可用隐藏 window 代替 | Rust        | Node.js        |
+| Webview  | System                     | System      | Chromium       |
+| 生态     | 前端                       | Rust + 前端 | Node.js + 前端 |
+| 上手难度 | 简单                       | 极难        | 困难           |
+
+Niva 使用了 Tauri 的跨端窗口管理库 tao 和跨端 Webview 库 wry，所以在窗口和 Webview 上面 Niva 和 Tauri 具备相同的能力，以及相同数量级的体积。不同之处在于，Niva 直接为 Webview 提供通用的 API，无需编写 Rust 代码，对前端开发者更加友好。而使用 Electron 则需要 Node.js 和 Chromium 的依赖，体积也更大。
+
+与 Electron 相比，Niva 不依赖 Node.js 和 Chromium，因此能够实现更小的体积，对前端开发者更加友好。Niva 提供了图形化的开发者工具和简单易用的配置，能够快速将前端项目迁移至 Niva 等桌面应用。
+
+总之，Niva 专注于提供更小的体积和更加便捷的开发体验，适合需要快速构建轻量级桌面应用的前端开发者使用。
+
+
+
--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -81,6 +81,10 @@ const config = {
             label: 'API 文档',
           },
           {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
             href: 'https://github.com/bramblex/niva/releases',
             label: '下载',
             position: 'right',
@@ -138,9 +142,40 @@ const config = {
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Niva, Inc. Built with Docusaurus.`,
       },
+
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'YOUR_APP_ID',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'YOUR_SEARCH_API_KEY',
+
+        indexName: 'YOUR_INDEX_NAME',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
       },
     }),
 };
