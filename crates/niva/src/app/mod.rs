0914afc33a6aa913434a4bc2aeccfeb1cac5c3d0
brambles <qjnight@gmail.com>
2023-06-07 09:14:21 +0800

    tmp

--- crates/niva/src/app/mod.rs
+++ crates/niva/src/app/mod.rs
@@ -1,6 +1,7 @@
 mod utils;
 
-use serde_json::{Value as JsonValue};
+use anyhow::{anyhow, Result};
+use serde_json::Value as JsonValue;
 use std::{collections::HashMap, path::PathBuf};
 
 pub struct NivaLaunchInfo {
@@ -10,38 +11,89 @@ pub struct NivaLaunchInfo {
 }
 
 pub struct NivaArguments {
-    niva_config_file: Option<JsonValue>,
-    niva_options: Option<JsonValue>,
-    niva_args: Vec<(String, JsonValue)>,
+    niva_file: Option<JsonValue>,
+    niva_base: Option<JsonValue>,
+    niva_options: Vec<(String, JsonValue)>,
 }
-impl NivaArguments { pub fn new() -> Self {
+
+impl NivaArguments {
+    fn is_str_start_with_digit(s: &str) -> bool {
+        if let Some(first_char) = s.chars().next() {
+            first_char.is_numeric()
+        } else {
+            false
+        }
+    }
+    fn parse_niva_arg<'a>(arg: &'a str) -> Option<(&'a str, JsonValue)> {
+        if !arg.starts_with("--") {
+            return None;
+        }
+
+        let arg = arg.trim_start_matches("--");
+        let mut parts = arg.splitn(2, '=');
+
+        let key_str = parts.next()?;
+        let value_str = parts.next();
+
+        if let Some(value_str) = value_str {
+            let mut value = JsonValue::Null;
+            if value_str == "null" {
+            } else if value_str == "true" {
+                value = JsonValue::Bool(true);
+            } else if value_str == "false" {
+                value = JsonValue::Bool(false);
+            } else if Self::is_str_start_with_digit(value_str) {
+                let result = value_str.parse::<f64>();
+                if let Ok(nu) = result {
+                    value = JsonValue::Number(nu);
+                } else {
+                    value = JsonValue::String(value_str.to_string());
+                }
+            } else {
+                value = JsonValue::String(value_str.to_string());
+            }
+            Some((key_str, value))
+        } else {
+            Some((key_str, JsonValue::Bool(true)))
+        }
+    }
+
+    pub fn new() -> Result<Self> {
         let args = std::env::args().collect::<Vec<String>>();
 
-        let mut niva_args = HashMap::<String, JsonValue>::new();
+        let mut niva_options: Vec<(String, JsonValue)> = vec![];
 
         for arg in args {
             if !arg.starts_with("--niva") {
                 continue;
             }
-            // let arg = arg.trim_start_matches("--niva");
-            // if arg == "--niva"
+
+            // let [key_str, value_str] = parts[..];
+
+            // if parts.len() != 2 {
+            //     return Err(anyhow!("Unexpected Niva Command line argument: {}", arg));
+            // }
+
+            // if key_str == "--niva-file" {
+            // } else if  key_str == "--niva" {
+            // } else if key {
+            // }
+
+            // if arg == "--niva-file" {
+            // } else if arg == "--niva" {
+            // } else {
+            //     let option_path = arg.trim_start_matches("--niva");
+            // }
             // if let Some((key, value)) = Self::parse_niva_arg() {
             //     niva_args.insert(key, value);
             // }
         }
 
-        Self {
-            niva_config_file: None,
-            niva_options: None,
-            niva_args: vec![],
-        }
-    }
-
-    pub fn patch(&self, options: &mut serde_json::Value) {
-    }
-
-    fn parse_niva_arg() -> Option<(String, JsonValue)> {
-        None
+        Ok(Self {
+            niva_file: None,
+            niva_base: None,
+            niva_options,
+        })
     }
 }
 
