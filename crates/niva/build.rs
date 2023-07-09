683a00150e9e481aeb12d6a884a35435d911a5fa
bramblex <qjnight@gmail.com>
2023-07-10 03:00:29 +0800

    feat: 提供更复杂的 js api

--- crates/niva/build.rs
+++ crates/niva/build.rs
@@ -1,14 +1,14 @@
-#[cfg(target_os = "windows")]
-extern crate winres;
-
-#[cfg(target_os = "windows")]
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.compile().unwrap();
-    build_version::write_version_file().expect("Failed to write version.rs file");
-}
+    // let profile = std::env::var("PROFILE").unwrap_or_else(|_| "debug".to_string());
+    // if profile == "debug" {
+    //     // Debug 构建配置
+    //     println!("Building in Debug mode");
+    //     // 在这里执行针对 Debug 构建的操作
+    // } else {
+    //     // Release 构建配置
+    //     println!("Building in Release mode");
+    //     // 在这里执行针对 Release 构建的操作
+    // }
 
-#[cfg(not(target_os = "windows"))]
-fn main() {
     build_version::write_version_file().expect("Failed to write version.rs file");
 }
